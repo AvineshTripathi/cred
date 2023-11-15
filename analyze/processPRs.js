cac27f58018c5ee43ee9baf7ecbb6cdd29ba1d1f
Avinesh Tripathi <73980067+AvineshTripathi@users.noreply.github.com>
2023-11-15 21:37:46 +0530

    Update processPRs.js

--- analyze/processPRs.js
+++ analyze/processPRs.js
@@ -56,8 +56,8 @@ async function processPRs() {
     });
 
     console.log(authorData)
-    console.log(authorJsonPath)
-    fs.writeFileSync(authorJsonPath, JSON.stringify(authorData, null, 2));
+    console.log(process.env.GITHUB_WORKSPACE+"/analyze/author.json")
+    fs.writeFileSync(process.env.GITHUB_WORKSPACE+"/analyze/author.json", JSON.stringify(authorData, null, 2));
     
     console.log('PRs processed and author.json updated successfully.');
   } catch (error) {
